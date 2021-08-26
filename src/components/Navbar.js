69af9501498acb3afdedd9a577c1d6b56923e6af
JoAnn R Siciliano <77905621+joannsiciliano@users.noreply.github.com>
2021-08-26 12:11:54 -0600

    navbar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import PClogo from "../images/pclogo.png";
 
 export default function Navbar() {
   return (
     <nav className="Navbar">
       <NavLink to="/" exact className="HomeButton">
-        Home
+        <img
+          src={PClogo}
+          className="logo"
+          alt="production club Home"
+          title="Home"
+        />
       </NavLink>
       <NavLink to="/projects">Projects</NavLink>
       <NavLink to="/team">Team</NavLink>
