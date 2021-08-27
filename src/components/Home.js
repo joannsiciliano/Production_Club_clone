0a15cb603e61f9c11d601a3e08b2046f140e7adf
JoAnn R Siciliano <77905621+joannsiciliano@users.noreply.github.com>
2021-08-27 11:37:52 -0600

    info and gif added to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
+import HomeCSS from "../cssModules/home.module.css";
+import pcgif from "../images/pc.gif";
 
 export default function Home() {
   return (
-    <div className="home">
-      <h1>Home</h1>
-    </div>
+    <section className={HomeCSS.homeContainer}>
+      <img src={pcgif} className={HomeCSS.backgroundGif} />
+      <div className={HomeCSS.homeDiv}>
+        <h1 className={HomeCSS.title}>Production Club</h1>
+        <p className={HomeCSS.paragraph}>
+          {" "}
+          Production Club is a multidisciplinary creative studio specialized in
+          designing and producing immersive experiences for the music, tech, and
+          gaming industries.
+        </p>
+      </div>
+    </section>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import PClogo from "../images/pclogo.png";
 import { Sling as Hamburger } from "hamburger-react";
 
 export default function Navbar() {
+  const [isOpen, setOpen] = useState(false);
+
+  // const handleToggle = (e) => {
+  //   e.preventDefault();
+  //   setOpen(!isOpen);
+  // };
   return (
     <nav className="Navbar">
       <img
@@ -12,17 +18,39 @@ export default function Navbar() {
         alt="production club Home"
         title="Home"
       />
-      <NavLink to="/" exact className="HomeButton">
-        Home
-      </NavLink>
-      <NavLink to="/projects">Projects</NavLink>
-      <NavLink to="/team">Team</NavLink>
-      <NavLink to="/downloads">Downloads</NavLink>
-      <NavLink to="/careers">Careers</NavLink>
-      <NavLink to="/shop">Shop</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
+      {/* <div className={`Links${isOpen} ? '-is-open' : '-closed' `}> */}
+      <div className="Links">
+        <NavLink to="/" exact className="NavButton">
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="NavButton">
+          Projects
+        </NavLink>
+        <NavLink to="/team" className="NavButton">
+          Team
+        </NavLink>
+        <NavLink to="/downloads" className="NavButton">
+          Downloads
+        </NavLink>
+        <NavLink to="/careers" className="NavButton">
+          Careers
+        </NavLink>
+        <NavLink to="/shop" className="NavButton">
+          Shop
+        </NavLink>
+        <NavLink to="/contact" className="NavButton">
+          Contact
+        </NavLink>
+      </div>
       <div className="Hamburger">
-        <Hamburger color="white" rounded />
+        <Hamburger
+          color="white"
+          rounded
+          easing="ease-in-out"
+          toggled={isOpen}
+          toggle={setOpen}
+          // onClick={(e) => handleToggle()}
+        />
       </div>
     </nav>
   );
